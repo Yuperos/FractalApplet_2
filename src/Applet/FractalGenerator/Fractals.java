0a7d06e8f0c7a7a2c4c1e1ebdd8c524bd3ae450a
Yuperos <Xenon-Pro@mail.ru>
2014-11-05 15:05:31 +0300

    Быстрый перенос на др. устройство

--- src/Applet/FractalGenerator/Fractals.java
+++ src/Applet/FractalGenerator/Fractals.java
@@ -1,16 +1,17 @@
 package Applet.FractalGenerator;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
 import java.util.List;
 
 public class Fractals {
 
-   private static int BRANCH_PYTHAGORAS = 1;
-   private static int BRANCH_ON_TRUNK = 2;
-   private static int BRANCH_FLUFF = 3;
-   private static int BRANCH_RHOMBUS_PARALLEL = 4;
+   final private static int BRANCH_PYTHAGORAS = 1;
+   final private static int BRANCH_ON_TRUNK = 2;
+   final private static int BRANCH_FLUFF = 3;
+   final private static int BRANCH_RHOMBUS_PARALLEL = 4;
 
    private int treeHeight;
-   private long genome;
    private Item root;
    private List<Item> lastGenerated;
    private static long RANDOMIZE_SEED = 489133282872437279L;
@@ -26,19 +27,44 @@ public class Fractals {
    public void setTreeHeight(int treeHeight) {
       this.treeHeight=treeHeight;
    }
-   private void assemblyBranchingGenome(int... a){
-      this.genome=0;
+
+   private long assemblyBranchingGenome(int... a){
+      long genome=0;
       if (a.length > 0)
          for(int i:a)
-            this.genome=i+this.genome*10;
+            genome=i+ genome *10;
+      return genome;
    }
 
    public void growEvergreen() {
-      this.assemblyBranchingGenome(BRANCH_ON_TRUNK, BRANCH_ON_TRUNK, BRANCH_FLUFF);
+      long genome = assemblyBranchingGenome(BRANCH_ON_TRUNK, BRANCH_ON_TRUNK, BRANCH_FLUFF);
       this.root.setSize(50,0,0,this.treeHeight);
       this.lastGenerated.add(root);
 
-      
+
+
+   }
+
+   private void nextBranch(long genome, int seed){
+      int currentGen = (int)genome%10;
+      genome/=10;
+
+//    TODO доделать алгоритмы в свичах
+
+      switch(currentGen){
+         case BRANCH_PYTHAGORAS: {
+            break;
+         }
+         case BRANCH_ON_TRUNK: {
+            break;
+         }
+         case BRANCH_FLUFF: {
+            break;
+         }
+         case BRANCH_RHOMBUS_PARALLEL: {
+            break;
+         }
+      }
 
    }
 }
