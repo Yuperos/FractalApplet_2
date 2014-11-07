07a960ebcfb045250f72930736b8cf1cf5cda461
Yuperos <Xenon-Pro@mail.ru>
2014-11-07 23:43:33 +0300

    Ветка для дерева пифагора в рекурсивном методе

--- src/Applet/FractalGenerator/Fractals.java
+++ src/Applet/FractalGenerator/Fractals.java
@@ -3,6 +3,7 @@ package Applet.FractalGenerator;
 import com.sun.xml.internal.bind.v2.TODO;
 
 import java.util.List;
+import java.util.Random;
 
 public class Fractals {
 
@@ -14,7 +15,8 @@ public class Fractals {
    private int treeHeight;
    private Item root;
    private List<Item> lastGenerated;
-   private static long RANDOMIZE_SEED = 489133282872437279L;
+   private static final long RANDOMIZE_SEED = 489133282872437279L;
+   private final Random seed = new Random(RANDOMIZE_SEED);
 
    public List<Item> getLastGenerated() {
       return lastGenerated;
@@ -41,21 +43,45 @@ public class Fractals {
       this.root.setSize(50,0,0,this.treeHeight);
       this.lastGenerated.add(root);
 
-
-
+      this.genNextBranch(genome, root);
    }
 
-   private void nextBranch(long genome, int seed){
+   private int randBranchCount(int maxCount) {
+      int branchCount=0;
+      int branchChanceSeed=(seed.nextInt((int)Math.pow(2,maxCount) - 1) ) + 1;
+      while((branchChanceSeed & 1)==1 || branchCount==0) {
+         branchCount+=branchChanceSeed & 1;
+         branchChanceSeed>>=1;
+         }
+      return branchCount;
+   }
+
+
+   private void genNextBranch(long genome, Item parentBranch){
       int currentGen = (int)genome%10;
       genome/=10;
 
-//    TODO доделать алгоритмы в свичах
+      // TODO доделать алгоритмы в свичах
 
       switch(currentGen){
          case BRANCH_PYTHAGORAS: {
+            int branchCount = randBranchCount(4);
+            for(int i=0; i<branchCount; i++) {
+               int nextBranchLengthFactor = seed.nextInt(15)* 5 + 10;
+               double nextBranchLength = parentBranch.getNextLength((double)nextBranchLengthFactor);
+               int nextBranchAngleFactor = seed.nextInt(10)* 8 - 40;
+               double nextBranchAngle = parentBranch.getNextAngleDegrees((double)nextBranchAngleFactor);
+               Item currentBranch = new Item((int)parentBranch.getEndX(), (int)parentBranch.getEndY(), nextBranchAngle, nextBranchLength);
+               lastGenerated.add(currentBranch);
+               genNextBranch(genome,currentBranch);
+               }
             break;
          }
          case BRANCH_ON_TRUNK: {
+            double tempLength = parentBranch.getNextLength(80.0);
+            while(tempLength > 3) {
+
+            }
             break;
          }
          case BRANCH_FLUFF: {
